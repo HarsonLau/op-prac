1b8e065d29d569801aee18232e3acff320311a50
harsonlau <harsonlau@gmail.com>
2019-11-22 14:28:55 +0800

    delete the buggy slice schedule policy

--- code/threads/list.h
+++ code/threads/list.h
@@ -62,7 +62,6 @@ class List {
     void SortedInsert(void *item, int sortKey);	// Put item into list
     void *SortedRemove(int *keyPtr); 	  	// Remove first item from list
 
-  private:
     ListElement *first;  	// Head of the list, NULL if list is empty
     ListElement *last;		// Last element of list
     int numInList;		// number of elements in list
